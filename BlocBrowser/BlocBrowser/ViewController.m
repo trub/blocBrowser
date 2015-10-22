11c80b4587f3b56c5423b0117347536b1830c7b7
matthew weintrub <heytrub@gmail.com>
2015-10-22 13:55:40 +0100

    adds URL field

--- BlocBrowser/BlocBrowser/ViewController.m
+++ BlocBrowser/BlocBrowser/ViewController.m
@@ -9,9 +9,10 @@
 #import "ViewController.h"
 #import <WebKit/Webkit.h>
 
-@interface ViewController () <WKNavigationDelegate>
+@interface ViewController () <WKNavigationDelegate, UITextFieldDelegate>
 
 @property (nonatomic, strong) WKWebView *webView;
+@property (nonatomic, strong) UITextField *textField;
 
 @end
 
@@ -23,6 +24,17 @@
     self.webView = [[WKWebView alloc] init];
     self.webView.navigationDelegate = self;
     
+    self.textField = [[UITextField alloc] init];
+    self.textField.keyboardType = UIKeyboardTypeURL;
+    self.textField.returnKeyType = UIReturnKeyDone;
+    self.textField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    self.textField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.textField.placeholder = NSLocalizedString(@"Website URL", @"Placeholder text");
+    self.textField.backgroundColor = [UIColor colorWithWhite:220/255.0f alpha:1];
+    self.textField.delegate = self;
+    
+    
+    
     
     NSString *urlString = @"http://espn.com";
     NSURL *url = [NSURL URLWithString: urlString];
@@ -31,19 +43,27 @@
     
     
     [mainView addSubview:self.webView];
+    [mainView addSubview:self.textField];
     self.view = mainView;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.edgesForExtendedLayout = UIRectEdgeNone;
 }
 
 - (void)viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
     
     //make the webview fill the main view
-    self.webView.frame = self.view.frame;
+//    self.webView.frame = self.view.frame;
+
+    static const CGFloat itemHeight = 50;
+    CGFloat width = CGRectGetWidth(self.view.bounds);
+    CGFloat browserHeight = CGRectGetHeight(self.view.bounds) - itemHeight;
+    self.textField.frame = CGRectMake(0, 0, width, itemHeight);
+    self.webView.frame = CGRectMake(0, CGRectGetMaxY(self.textField.frame), width, browserHeight);
     
 }
 
